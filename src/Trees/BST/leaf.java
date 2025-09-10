30a60b898272824788dacabea344b2aa03dbe899
Divyansh Patel <pateldivyansh131@gmail.com>
2025-09-10 10:25:44 +0530

    Tree

---
+++ src/Trees/BST/leaf.java
@@ -0,0 +1,23 @@
+package Trees.BST;
+
+public class leaf {
+    static class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val){
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void main(String args[]){
+        int values[] = { 5 , 1 , 3, 4, 2, 7};
+
+    }
+    public static Node insert(int val){
+        if(val == null) return ;
+        if(root.val >)
+    }
+    public static void paths(Node root)
+}
