842201de16e12e84e815a448b400ef1edc9e4b64
Divyansh <pateldivyansh131@gmail.com>
2026-02-19 02:19:53 +0530

    BT

--- src/BackTracking/Total_queens.java
+++ src/BackTracking/Total_queens.java
@@ -6,10 +6,12 @@ public class Total_queens {
         int tq = 2;
         int n = 4;
         boolean board[] = new boolean[n];
-        back(board , tq ,0  , "");
+        //permutation(board , tq ,0  , "");
+        //System.out.println("Total possible way:"+c);
+        combination(board ,tq , 0 , "" , 0);
         System.out.println("Total possible way:"+c);
     }
-    public static void back(boolean[] board , int tq , int tqsf , String ans){
+    public static void permutation(boolean[] board , int tq , int tqsf , String ans){
         if(tqsf == tq){
             c++;
             System.out.println(ans);
@@ -19,7 +21,24 @@ public class Total_queens {
         for(int i = 0;i<board.length;i++){
             if(board[i] ==false){
                 board[i] = true;
-                back(board , tq , tqsf+1 , ans + "b" + i + "q" + tqsf);
+                permutation(board , tq , tqsf+1 , ans + "b" + i + "q" + tqsf);
+                board[i] = false; // undo (main thing to complete the backtrack)
+            }
+        }
+    }
+
+
+    public static void combination(boolean[] board , int tq , int tqsf , String ans ,int idx){
+        if(tqsf == tq){
+            c++;
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i = idx;i<board.length;i++){
+            if(board[i] ==false){
+                board[i] = true;
+                combination(board , tq , tqsf+1 , ans + "b" + i + "q" + tqsf , i + 1);
                 board[i] = false; // undo (main thing to complete the backtrack)
             }
         }
