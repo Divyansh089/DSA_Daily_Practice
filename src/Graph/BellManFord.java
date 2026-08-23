ac66a1e4cd02ca0d04d06c60adb70a3a47e6947b
Divyansh Patel <pateldivyansh131@gmail.com>
2026-08-23 14:05:14 +0530

    BMF

--- src/Graph/BellManFord.java
+++ src/Graph/BellManFord.java
@@ -1,65 +1,62 @@
 package Graph;
 
 import java.util.*;
-
-
 public class BellManFord {
-    private HashMap<Integer, HashMap<Integer, Integer>> map;
+    private HashMap<Integer , HashMap<Integer , Integer>> map;
 
-    public BellManFord(int v) {
-        map = new HashMap<>();
-        for (int i = 1; i <= v; i++) {
-            map.put(i, new HashMap<>());
+    public BellManFord(int v){ // number of vertex
+        map  = new HashMap<>();
+        for(int i =1;i<=v;i++){
+            map.put(i , new HashMap<>());
         }
     }
-
-    public void AddEdge(int v1, int v2, int cost) {
-        map.get(v1).put(v2, cost);
+    public  void AddEdge(int v1 , int v2 , int cost){
+        map.get(v1).put(v2 , cost);
 
     }
-
     class EdgePair{
-        int e1 , e2 , cost;
-        public EdgePair(int e1 , int e2 , int cost){
-            this.e1 = e1;
-            this.e2 = e2;
+        int v1;
+        int v2;
+        int cost;
+        public EdgePair(int v1 , int v2 , int cost){
+            this.v1 = v1;
+            this.v2 = v2;
             this.cost = cost;
         }
     }
 
     public List<EdgePair> getAllEdge(){
         List<EdgePair> ll = new ArrayList<>();
-        for(int e1 : map.keySet()){
-            for(int e2: map.get(e1).keySet()){
-                int cost = map.get(e1).get(e2);
-                ll.add(new EdgePair(e1 , e2 , cost));
+        for(int v1: map.keySet()){
+            for(int v2: map.get(v1).keySet()){
+                int cost = map.get(v1).get(v2);
+                ll.add(new EdgePair(v1 , v2 ,cost));
             }
         }
         return ll;
     }
 
-    public void BellManFord(){
+    public void BellMan_Ford(){
         int v = map.size();
-        int [] dp = new int[v+1];
-
-        //src == 1
-        for(int i  = 2;i < dp.length;i++){
-            dp[i] = 9999999;
+        int [] dis = new int[v +1];
+        for(int i = 2;i< dis.length;i++){ // no. of vertex start from 1 & 1 to 1 dist. is 0  , that's why 2
+            dis[i] = 99999999;
         }
         List<EdgePair> ll = getAllEdge();
-        for(int i = 1;i <=v;i++){
-            for(EdgePair e:ll){
-                if( i ==v && dp[e.e2] > dp[e.e1] + e.cost){
-                    System.out.println("-ve wt ka cycle");
-                    return ;
+        for(int i = 1 ;i<=v;i++){
+            for(EdgePair ep : ll){
+                if(i == v && dis[ep.v2] > dis[ep.v1] + ep.cost){ // -ve weight cycle detection
+                    System.out.println("-ve weight ka cycle hai");
+                    return;
                 }
-                if(dp[e.e2] > dp[e.e1] + e.cost){
-                    dp[e.e2] = dp[e.e1] +e.cost;
+                if(dis[ep.v2] > dis[ep.v1] + ep.cost){ // relaxing edge
+                    dis[ep.v2] = dis[ep.v1] + ep.cost;
                 }
             }
         }
-        for(int i = 1;i<dp.length;i++){
-            System.out.println(dp[i] + " ");
+
+        for(int i  = 1;i<=v;i++){ // printing minimum dist.
+            System.out.print(dis[i] +  " ");
         }
     }
 
@@ -72,6 +69,6 @@ public class BellManFord {
         bf.AddEdge(5, 2, 1);
         bf.AddEdge(3, 4, -3);
         bf.AddEdge(4, 5, 4);
-        bf.BellManFord();
+        bf.BellMan_Ford();
     }
 }
